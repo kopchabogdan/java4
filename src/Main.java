9f0c26fac47a7f50a8da772100b55a39cb0f9f3f
Bogdan Kopcha <kopchabogdan@gmail.com>
2022-10-11 14:05:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String s="Some text";
+        System.out.println("TASK 1: " + s.toUpperCase());
+
+        System.out.println("TASK 2: " + "\uD83D\uDE43 \uD83D\uDE08 \uD83E\uDDC0");
+
+        String lr = "Лабораторна робота";
+        String four = "4";
+        String result = lr + " " + four;
+        System.out.println("TASK 3: " + result);
+    }
+}
